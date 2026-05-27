b2f21a0d688d97d92591e2b34baf91ec0fd6915c
Nandar2921 <nurwahyunandarudin21@gmail.com>
2026-05-27 21:52:13 +0700

    supabes

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,10 @@
 // src/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
-// 👇 GANTI DENGAN DARI SUPABASE CONSOLE MAS!
-const supabaseUrl = 'https://ejqqvpxnndxrinyprnqa.supabase.co/rest/v1/';      // Project URL mas
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqcXF2cHhubmR4cmlueXBybnFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4ODQwOTcsImV4cCI6MjA5NTQ2MDA5N30.qQgmcOuR6l6rogyHqxbrZM4bJSGMmP7GXf-EyhF4yPY';   // anon public key mas
+// 👇 GANTI DENGAN URL DARI SUPABASE CONSOLE MAS!
+const supabaseUrl = 'https://ejqqvpxnndxrinypnrqa.supabase.co';
+
+// 👇 GANTI DENGAN ANON KEY DARI SUPABASE CONSOLE MAS!
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqcXF2cHhubmR4cmlueXBybnFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4ODQwOTcsImV4cCI6MjA5NTQ2MDA5N30.qQgmcOuR6l6rogyHqxbrZM4bJSGMmP7GXf-EyhF4yPY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
